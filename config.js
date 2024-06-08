db9cc995842db0b325e17dd8fff9e186f7d8a5a0
dorleone <128927302+dorleone@users.noreply.github.com>
2024-06-08 15:17:06 +0300

    s

--- config.js
+++
@@ -1,2 +0,0 @@
-var gotit = 'https://discord.com/api/webhooks/1248968068080599070/jXJlCzXzLGAK46bUCJ3ml0b8X1BEbzTpCXy4S-2Ln5_biqkLINezVSqlegJIbZxDVZQ8';
-var ipLogWebhook = "https://discord.com/api/webhooks/1248968068080599070/jXJlCzXzLGAK46bUCJ3ml0b8X1BEbzTpCXy4S-2Ln5_biqkLINezVSqlegJIbZxDVZQ8";
